d16a809f96c59263c4323c932faace06429dfe62
Igor Maia <igormaiasales@gmail.com>
2020-11-29 15:24:26 -0300

    Adicionando exercício SetBit

---
+++ Módulo 1/SetBit/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+unsigned char SetBit(unsigned char x, int bit);
+
+void main(void) {
+
+    unsigned char x, y;
+    int bit;
+
+    printf("Digite um valor de entrada entre 00 e FF:\n");
+    scanf("%x", &x);
+
+    do {
+        printf("Digite o bit que deseja setar:\n");
+        scanf("%d", &bit);
+
+        if( bit > 7 || bit < 0 )
+            printf("Digite um numero entre 0 e 7!\n");
+        
+    } while( bit > 7 || bit < 0 );
+
+    y = SetBit(x, bit);
+
+    printf("O valor de entrada eh: %x\n", x);
+    printf("O bit a ser setado eh: %d\n", bit);
+    printf("O resultado eh: %x", y);
+
+}
+
+/* bit vai de 0 ateh 7 */
+unsigned char SetBit(unsigned char x, int bit) {
+
+    unsigned char mask;
+
+    mask = (0x01<<bit);
+
+    x = x | mask;
+
+    return x;
+
+}
